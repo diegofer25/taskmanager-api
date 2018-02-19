195b4633a4adf4e14c598db1130f186e9db5efe0
Diego Lamarão <diego.lamarao92@outlook.com>
2018-02-19 20:35:05 -0300

    Adicionar arquivos de projeto.

---
+++ taskmanager-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="taskmanager_api.WebApiApplication" Language="C#" %>
---
+++ taskmanager-api/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do seguinte 
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("taskmanager_api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("taskmanager_api")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM. Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O seguinte GUID é para a ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("42eb334a-83b9-425a-a942-5575e45eacf5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// utilizando o "*" como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
