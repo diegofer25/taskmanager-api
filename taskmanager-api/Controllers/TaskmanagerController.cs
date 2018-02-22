7fd3d21897b8c33428865f4a22b00b6d5856e171
Diego Lamarão <diego.lamarao92@outlook.com>
2018-02-22 12:58:20 -0300

    v1.0.0

--- taskmanager-api/Controllers/TaskmanagerController.cs
+++ taskmanager-api/Controllers/TaskmanagerController.cs
@@ -2,45 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 using taskmanager_api.Models;
-using Swashbuckle.Swagger.Annotations;
 
 namespace taskmanager_api.Controllers
 {
+    [Route("api/Taskmanager")]
     public class TaskmanagerController : ApiController
     {
         taskmanagerdb tmdb = new taskmanagerdb();
-        // GET: api/Taskmanager
-        public List<tarefas> Get()
+
+        public IEnumerable<tarefas> GetAll()
         {
             return tmdb.tarefas.ToList();
         }
 
-        // Post: api/Taskmanager
-        [HttpPost]
-        public void DeleteById(int id)
+        public IHttpActionResult GatTask(int id)
         {
-            tmdb.tarefas.Remove(tmdb.tarefas.Find(id));
-            tmdb.SaveChanges();
+            tarefas tarefa = tmdb.tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+            return Ok(tarefa);
         }
-        /*
-        // POST: api/Taskmanager
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Taskmanager/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Taskmanager/5
-        public void Delete(int id)
-        {
-        }
-        */
     }
 }
